39b0ed1082243f14e6dee51e6767540e6aa3f26f
msrks <mshrrks@gmail.com>
2018-03-14 16:46:16 +0900

    fix deployment setting

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
-#cp -r dist/ docs/
-#cp index.html docs/
-#sed -i -e 's@/dist/@@g' docs/index.html
-#rm docs/index.html-e
-
 mkdir docs
 cp -rf dist/ docs/dist/
 cp index.html docs/
+sed -i -e 's@/dist/@dist/@g' docs/index.html
+rm docs/index.html-e
